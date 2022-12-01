5d1a656cfc5a59da80d7d918f2548d38091f99e2
Test1 1 <williamh2461@gmail.com>
2022-11-30 21:14:01 -0500

    finished the reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -8,7 +8,20 @@ const reactionSchema = new Schema(
             default: () => new Types.ObjectId
         },
         reactionBody: {
-
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: timestamp => dateFormat(timestamp)
         }
     }
 )
+
+module.exports = reactionSchema;
