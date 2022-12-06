c826bc38532fb4099708f008f6aa3ead0e6958ce
Test1 1 <williamh2461@gmail.com>
2022-12-05 23:28:44 -0500

    added put route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,10 +4,11 @@ const {
     getSingleUser,
     createUser,
     deleteUser,
+    updateUser,
 } = require('../../controllers/userController')
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
 module.exports = router;   
